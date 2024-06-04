37c7215fb8e9c9b76569fbc6d00cbf34aa1b73ea
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-06-04 15:43:18 +0700

    update show menu js

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// =========== Show List Toggle==============
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show__menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show__menu')
+    })
+}
